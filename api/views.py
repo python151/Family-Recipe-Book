fdc9299fd02866b22e6df0249c510a00c186c49d
Matthew Miller <39663150+python151@users.noreply.github.com>
2020-04-05 15:37:56 -0500

    Fixed API Response

--- api/views.py
+++ api/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from frontend.models import Meal, Tag, Ingredient
 from django.core import serializers
+from django.http import JsonResponse
 
 
 # Create your views here.
-def getDish(request, id):
+def getDish(request):
     meals = Meal.objects.all()
-    data = serializers('json', meals)
-    return HttpResponse(request, data)
+    ret = []
+    for meal in meals:
+        ret.append({
+            "name": meal.name,
+            "description": meal.description,
+            "tags": [tag.name for tag in meal.tags.all()],
+            "ingridiant": [ingridiant.name for ingridiant in meal.ingredients.all()],
+        })
+    return JsonResponse({"data":ret})
 
 def editDish(request, id):
     data = {}
